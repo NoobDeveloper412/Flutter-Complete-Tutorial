ad5abc398bdcade54a52f79d11823638dfbd96ae
NoobDeveloper412 <mzuhair412@gmail.com>
2022-02-03 11:39:57 +0500

    Added images to the scaffold using internet.

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,11 @@ void main() {
     home: Scaffold(
       appBar: AppBar(title:Text('First App!'),
       backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage('https://tse1.mm.bing.net/th?id=OIP.Wytlw5AmN2HoCJ_kLGF1EgHaF7&pid=Api&P=0&w=201&h=161', scale:1.0),
+        ),
       )
     )
   ));
